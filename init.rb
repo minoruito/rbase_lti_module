7548cde1862a848e4616dcabaf3875e9a79ca937
Minoru Ito <minoru@i-do-inc.jp>
2026-06-13 03:06:47 +0900

    開発中

--- init.rb
+++ init.rb
@@ -1,6 +1,10 @@
 # Include hook code here
 require './lib/rbase/plugin_module'
 
+# SessionsControllerExt が include する Lti::OperationLogCreation を、
+# autoload より先に読み込む（constantize 時の NameError を防ぐ）
+require File.expand_path("app/controllers/concerns/lti/operation_log_creation", __dir__)
+
 Rbase::PluginModule.register(
   "RbaseLtiModule::SystemSettingExt",
   "RbaseLtiModule::AdminUserExt",
